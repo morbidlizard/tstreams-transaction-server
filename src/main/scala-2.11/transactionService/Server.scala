d7e627e43af5531e865527cc1f89e45ceadd9245
Aleksandr <ognelisar@gmail.com>
2016-11-09 16:20:09 +0700

    StreamService is developed

--- src/main/scala-2.11/transactionService/Server.scala
+++ src/main/scala-2.11/transactionService/Server.scala
@@ -5,14 +5,14 @@ import com.twitter.util.Await
 import transactionService.impl.{StreamServiceImpl, TransactionMetaServiceImpl}
 
 object Server extends App {
-//  private class ThriftServer extends StreamServiceImpl with TransactionMetaServiceImpl
-//
-//  val server = Thrift.server
-//  val iface1 = server.serveIface("localhost:8080", new ThriftServer)
-//  val iface2 = server.serveIface("localhost:8081", new ThriftServer)
-//
-//  Await.ready(iface2)
-//  Await.ready(iface1.close())
+  private class ThriftServer extends StreamServiceImpl
+
+  val server = Thrift.server
+  val iface1 = server.serveIface("localhost:8080", new ThriftServer)
+  val iface2 = server.serveIface("localhost:8081", new ThriftServer)
+
+  Await.ready(iface2)
+  Await.ready(iface1.close())
   //#thriftserverapi
 
 }

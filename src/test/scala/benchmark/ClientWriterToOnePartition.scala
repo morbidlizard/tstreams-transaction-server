f992052179c99ad29f5c50ac8be6ae7a1605b7c2
mikhaljova_ka <buterffly@sibmail.com>
2016-12-05 10:53:33 +0700

    add benchmark test (multiple clients - one server)

---
+++ src/test/scala/benchmark/ClientWriterToOnePartition.scala
@@ -0,0 +1,39 @@
+package benchmark
+
+import com.twitter.util.Await
+import transactionService.rpc.TransactionStates
+import transactionZookeeperService.TransactionZooKeeperClient
+
+object ClientWriterToOnePartition extends TransactionCreator with CsvWriter with TimeMeasure {
+  private val rand = new scala.util.Random()
+  private val streamName = "one-partition"
+
+  def main(args: Array[String]) {
+    val filename = rand.nextInt(1000) + "clientOneServerBenchmark.csv"
+    val txnCount = args(0).toInt
+    val dataSize = args(1).toInt
+    val client = new TransactionZooKeeperClient
+    Await.result(client.putStream(streamName, 1, None, 5))
+
+    val producerTransactions = createProducerTransactions(streamName, TransactionStates.Opened, 1)
+
+    Await.result(client.putTransactions(producerTransactions, Seq()))
+
+    val data = createTransactionData(dataSize)
+
+    var globalProgress = 1
+    val result = (1 to txnCount).map(x => {
+      val localProgress = (x.toDouble / txnCount * 100).round
+      if (globalProgress == localProgress) {
+        println(localProgress + "%")
+        globalProgress += 1
+      }
+
+      x -> time(Await.result(client.putTransactionData(producerTransactions.head, data)))
+    })
+
+    writeTransactionsAndTime(filename, result)
+
+    Await.result(client.delStream(streamName))
+  }
+}
--- src/test/scala/benchmark/Installer.scala
+++ src/test/scala/benchmark/Installer.scala
@@ -7,22 +7,25 @@ import configProperties.DB
 import org.apache.commons.io.FileUtils
 import transactionZookeeperService.TransactionZooKeeperServer
 
-object Installation {
+trait Installer {
   def clearDB() = {
-    FileUtils.deleteDirectory(new File(DB.StreamDirName))
-    FileUtils.deleteDirectory(new File(DB.TransactionDataDirName))
-    FileUtils.deleteDirectory(new File(DB.TransactionMetaDirName))
+    FileUtils.deleteDirectory(new File(DB.PathToDatabases + "/" + DB.StreamDirName))
+    FileUtils.deleteDirectory(new File(DB.PathToDatabases + "/" + DB.TransactionDataDirName))
+    FileUtils.deleteDirectory(new File(DB.PathToDatabases + "/" + DB.TransactionMetaDirName))
   }
 
   def startAuthServer() = {
     new Thread(new Runnable {
       LogManager.getLogManager.reset()
+
       override def run(): Unit = authService.AuthServer.main(Array())
     }).start()
   }
+
   def startTransactionServer() = {
     new Thread(new Runnable {
       LogManager.getLogManager.reset()
+
       override def run(): Unit = TransactionZooKeeperServer.main(Array())
     }).start()
   }

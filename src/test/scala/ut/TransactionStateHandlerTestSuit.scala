27ade6292c88a6f5a90406429d2cbe62086fb400
mikhaljova_ka <buterffly@sibmail.com>
2017-03-17 09:35:18 +0700

    3422 have done unit testing of TransactionStateHandler

--- src/test/scala/ut/TransactionStateHandlerTestSuit.scala
+++ src/test/scala/ut/TransactionStateHandlerTestSuit.scala
@@ -4,6 +4,7 @@ import com.bwsw.tstreamstransactionserver.netty.server.transactionMetadataServic
 import com.bwsw.tstreamstransactionserver.netty.server.transactionMetadataService.stateHandler.TransactionStateHandler
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 import transactionService.rpc.TransactionStates
+import transactionService.rpc.TransactionStates._
 
 class TransactionStateHandlerTestSuit extends FlatSpec with Matchers with BeforeAndAfterAll {
   //arrange
@@ -17,70 +18,290 @@ class TransactionStateHandlerTestSuit extends FlatSpec with Matchers with Before
 
   it should "not put producerTransaction with state: Checkpointed. " +
     "It should throw an exception (due an invalid transition of state machine)" in {
+    //arrange
+    val producerTransaction = createProducerTransaction(Checkpointed, ts)
 
-    val producerTransactionKey = createProducerTransactionKey(TransactionStates.Checkpointed, ts)
-
+    //act and assert
     assertThrows[IllegalArgumentException] {
-      transactionStateHandler.transitProducerTransactionToNewState(Seq(producerTransactionKey))
+      transactionStateHandler.transitProducerTransactionToNewState(Seq(producerTransaction))
     }
   }
 
   it should "not put producerTransaction with state: Invalid. " +
     "It should throw an exception (due an invalid transition of state machine)" in {
+    //arrange
+    val producerTransaction = createProducerTransaction(Invalid, ts)
 
-    val producerTransactionKey = createProducerTransactionKey(TransactionStates.Invalid, ts)
-
+    //act and assert
     assertThrows[IllegalArgumentException] {
-      transactionStateHandler.transitProducerTransactionToNewState(Seq(producerTransactionKey))
+      transactionStateHandler.transitProducerTransactionToNewState(Seq(producerTransaction))
     }
   }
 
   it should "not put producerTransaction with state: Cancel. " +
     "It should throw an exception (due an invalid transition of state machine)" in {
+    //arrange
+    val producerTransaction = createProducerTransaction(Cancel, ts)
 
-    val producerTransactionKey = createProducerTransactionKey(TransactionStates.Cancel, ts)
-
+    //act and assert
     assertThrows[IllegalArgumentException] {
-      transactionStateHandler.transitProducerTransactionToNewState(Seq(producerTransactionKey))
+      transactionStateHandler.transitProducerTransactionToNewState(Seq(producerTransaction))
     }
   }
 
-  it should "process the following chain of states of producer transactions: Opened -> Checkpointed" in {
-    val openedProducerTransactionKey = createProducerTransactionKey(TransactionStates.Opened, ts)
-    val checkpointedProducerTransactionKey = createProducerTransactionKey(TransactionStates.Checkpointed, ts + 1)
+  it should "not put producerTransaction with state: Updated. " +
+    "It should throw an exception (due an invalid transition of state machine)" in {
+    //arrange
+    val producerTransaction = createProducerTransaction(Updated, ts)
 
-    transactionStateHandler.transitProducerTransactionToNewState(Seq(openedProducerTransactionKey, checkpointedProducerTransactionKey))
+    //act and assert
+    assertThrows[IllegalArgumentException] {
+      transactionStateHandler.transitProducerTransactionToNewState(Seq(producerTransaction))
+    }
   }
 
-  it should "process the following chain of states of producer transactions: Opened -> Updated -> Updated -> Checkpointed" in {
-    val openedProducerTransactionKey = createProducerTransactionKey(TransactionStates.Opened, ts)
-    val updatedProducerTransactionKey1 = createProducerTransactionKey(TransactionStates.Opened, ts + 1)
-    val updatedProducerTransactionKey2 = createProducerTransactionKey(TransactionStates.Opened, ts + 2)
-    val checkpointedProducerTransactionKey = createProducerTransactionKey(TransactionStates.Checkpointed, ts + 3)
+  it should "not process the following chain of states of producer transactions: Opened -> Invalid. " +
+    "It should throw an exception (due an invalid transition of state machine)" in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+    val invalidProducerTransaction = createProducerTransaction(Invalid, ts + 1)
 
-    transactionStateHandler.transitProducerTransactionToNewState(
-      Seq(openedProducerTransactionKey,
-        updatedProducerTransactionKey1,
-        updatedProducerTransactionKey2,
-        checkpointedProducerTransactionKey
+    //act and assert
+    assertThrows[IllegalArgumentException] {
+      transactionStateHandler.transitProducerTransactionToNewState(
+        Seq(
+          openedProducerTransaction,
+          invalidProducerTransaction
+        ))
+    }
+  }
+
+  it should "not process the following chain of states of producer transactions: Opened -> Updated -> Updated -> Invalid. " +
+    "It should throw an exception (due an invalid transition of state machine)" in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+    val updatedProducerTransaction1 = createProducerTransaction(Updated, ts + 1)
+    val updatedProducerTransaction2 = createProducerTransaction(Updated, ts + 2)
+    val invalidProducerTransaction = createProducerTransaction(Invalid, ts + 3)
+
+    //act and assert
+    assertThrows[IllegalArgumentException] {
+      transactionStateHandler.transitProducerTransactionToNewState(
+        Seq(
+          openedProducerTransaction,
+          updatedProducerTransaction1,
+          updatedProducerTransaction2,
+          invalidProducerTransaction
+        ))
+    }
+  }
+
+  it should "process the following chain of states of producer transactions: Opened -> Checkpointed. " +
+    "The final state of transaction should be Checkpointed" in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+    val checkpointedProducerTransaction = createProducerTransaction(Checkpointed, ts + 1)
+
+    //act
+    val finalState = transactionStateHandler.transitProducerTransactionToNewState(
+      Seq(openedProducerTransaction,
+        checkpointedProducerTransaction
       ))
+
+    //assert
+    finalState.state shouldBe Checkpointed
   }
 
-  it should "process the following chain of states of producer transactions: Opened -> Updated -> Updated -> Cancel" in {
-    val openedProducerTransactionKey = createProducerTransactionKey(TransactionStates.Opened, ts)
-    val updatedProducerTransactionKey1 = createProducerTransactionKey(TransactionStates.Opened, ts + 1)
-    val updatedProducerTransactionKey2 = createProducerTransactionKey(TransactionStates.Opened, ts + 2)
-    val checkpointedProducerTransactionKey = createProducerTransactionKey(TransactionStates.Cancel, ts + 3)
+  it should "process the following chain of states of producer transactions: Opened -> Updated -> Updated -> Checkpointed. " +
+    "The final state of transaction should be Checkpointed" in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+    val updatedProducerTransaction1 = createProducerTransaction(Updated, ts + 1)
+    val updatedProducerTransaction2 = createProducerTransaction(Updated, ts + 2)
+    val checkpointedProducerTransaction = createProducerTransaction(Checkpointed, ts + 3)
 
-    transactionStateHandler.transitProducerTransactionToNewState(
-      Seq(openedProducerTransactionKey,
-        updatedProducerTransactionKey1,
-        updatedProducerTransactionKey2,
-        checkpointedProducerTransactionKey
+    //act
+    val finalState = transactionStateHandler.transitProducerTransactionToNewState(
+      Seq(openedProducerTransaction,
+        updatedProducerTransaction1,
+        updatedProducerTransaction2,
+        checkpointedProducerTransaction
       ))
+
+    //assert
+    finalState.state shouldBe Checkpointed
   }
 
-  private def createProducerTransactionKey(transactionState: TransactionStates, ts: Long) = {
+  it should "process the following chain of states of producer transactions: Opened -> Updated -> Checkpointed -> Cancel. " +
+    "The final state of transaction should be Checkpointed, because it should ignore the part of chain following after Checkpointed state." in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+    val updatedProducerTransaction = createProducerTransaction(Updated, ts + 1)
+    val checkpointedProducerTransaction = createProducerTransaction(Checkpointed, ts + 2)
+    val cancelProducerTransaction = createProducerTransaction(Cancel, ts + 3)
+
+    //act
+    val finalState = transactionStateHandler.transitProducerTransactionToNewState(
+      Seq(openedProducerTransaction,
+        updatedProducerTransaction,
+        checkpointedProducerTransaction,
+        cancelProducerTransaction
+      ))
+
+    //assert
+    finalState.state shouldBe Checkpointed
+  }
+
+  it should "process the following chain of states of producer transactions: Opened -> Updated -> Checkpointed -> Invalid. " +
+    "The final state of transaction should be Checkpointed, because it should ignore the part of chain following after Checkpointed state." in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+    val updatedProducerTransaction = createProducerTransaction(Updated, ts + 1)
+    val checkpointedProducerTransaction = createProducerTransaction(Checkpointed, ts + 2)
+    val cancelProducerTransaction = createProducerTransaction(Invalid, ts + 3)
+
+    //act
+    val finalState = transactionStateHandler.transitProducerTransactionToNewState(
+      Seq(openedProducerTransaction,
+        updatedProducerTransaction,
+        checkpointedProducerTransaction,
+        cancelProducerTransaction
+      ))
+
+    //assert
+    finalState.state shouldBe Checkpointed
+  }
+
+  it should "process the following chain of states of producer transactions: Opened -> Updated -> Checkpointed -> Opened -> Cancel. " +
+    "The final state of transaction should be Checkpointed, because it should ignore the part of chain following after Checkpointed state." in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+    val updatedProducerTransaction = createProducerTransaction(Updated, ts + 1)
+    val checkpointedProducerTransaction = createProducerTransaction(Checkpointed, ts + 2)
+    val openedProducerTransaction2 = createProducerTransaction(Opened, ts + 3)
+    val cancelProducerTransaction = createProducerTransaction(Cancel, ts + 4)
+
+    //act
+    val finalState = transactionStateHandler.transitProducerTransactionToNewState(
+      Seq(openedProducerTransaction,
+        updatedProducerTransaction,
+        checkpointedProducerTransaction,
+        openedProducerTransaction2,
+        cancelProducerTransaction
+      ))
+
+    //assert
+    finalState.state shouldBe Checkpointed
+  }
+
+  it should "process the following chain of states of producer transactions: Opened -> Cancel. " +
+    "The final state of transaction should be Invalid" in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+    val cancelProducerTransaction = createProducerTransaction(Cancel, ts + 1)
+
+    //act
+    val finalState = transactionStateHandler.transitProducerTransactionToNewState(
+      Seq(openedProducerTransaction,
+        cancelProducerTransaction
+      ))
+
+    //assert
+    finalState.state shouldBe Invalid
+  }
+
+  it should "process the following chain of states of producer transactions: Opened -> Updated -> Updated -> Cancel." +
+    "The final state of transaction should be Invalid" in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+    val updatedProducerTransaction1 = createProducerTransaction(Updated, ts + 1)
+    val updatedProducerTransaction2 = createProducerTransaction(Updated, ts + 2)
+    val cancelProducerTransaction = createProducerTransaction(Cancel, ts + 3)
+
+    //act
+    val finalState = transactionStateHandler.transitProducerTransactionToNewState(
+      Seq(openedProducerTransaction,
+        updatedProducerTransaction1,
+        updatedProducerTransaction2,
+        cancelProducerTransaction
+      ))
+
+    //assert
+    finalState.state shouldBe Invalid
+  }
+
+  it should "process the following chain of states of producer transactions: Opened -> Updated -> Cancel -> Checkpointed. " +
+    "The final state of transaction should be Invalid because it should ignore the part of chain following after Cancel state" in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+    val updatedProducerTransaction1 = createProducerTransaction(Updated, ts + 1)
+    val cancelProducerTransaction = createProducerTransaction(Cancel, ts + 2)
+    val checkpointedProducerTransaction = createProducerTransaction(Checkpointed, ts + 3)
+
+    //act
+    val finalState = transactionStateHandler.transitProducerTransactionToNewState(
+      Seq(openedProducerTransaction,
+        updatedProducerTransaction1,
+        cancelProducerTransaction,
+        checkpointedProducerTransaction
+      ))
+
+    //assert
+    finalState.state shouldBe Invalid
+  }
+
+  it should "process the following chain of states of producer transactions: Opened -> Updated -> Cancel -> Opened -> Checkpointed. " +
+    "The final state of transaction should be Invalid  because it should ignore the part of chain following after Cancel state" in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+    val updatedProducerTransaction1 = createProducerTransaction(Updated, ts + 1)
+    val cancelProducerTransaction = createProducerTransaction(Cancel, ts + 2)
+    val openedProducerTransaction2 = createProducerTransaction(Opened, ts + 3)
+    val checkpointedProducerTransaction = createProducerTransaction(Checkpointed, ts + 4)
+
+    //act
+    val finalState = transactionStateHandler.transitProducerTransactionToNewState(
+      Seq(openedProducerTransaction,
+        updatedProducerTransaction1,
+        cancelProducerTransaction,
+        openedProducerTransaction2,
+        checkpointedProducerTransaction
+      ))
+
+    //assert
+    finalState.state shouldBe Invalid
+  }
+
+  it should "process the case in which a producer transaction remains in Opened state" in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+
+    //act
+    val finalState = transactionStateHandler.transitProducerTransactionToNewState(Seq(openedProducerTransaction))
+
+    //assert
+    finalState.state shouldBe Opened
+  }
+
+  it should "process the following chain of states of producer transactions: Opened -> Updated -> Updated. " +
+    "The final state of transaction should be Opened" in {
+    //arrange
+    val openedProducerTransaction = createProducerTransaction(Opened, ts)
+    val updatedProducerTransaction1 = createProducerTransaction(Updated, ts + 1)
+    val updatedProducerTransaction2 = createProducerTransaction(Updated, ts + 2)
+
+    //act
+    val finalState = transactionStateHandler.transitProducerTransactionToNewState(
+      Seq(openedProducerTransaction,
+        updatedProducerTransaction1,
+        updatedProducerTransaction2
+      ))
+
+    //assert
+    finalState.state shouldBe Opened
+  }
+
+  private def createProducerTransaction(transactionState: TransactionStates, ts: Long) = {
     val producerTransaction = transactionService.rpc.ProducerTransaction(streamName, streamPartitions, ts, transactionState, quantity, openedTTL)
 
     ProducerTransactionKey(producerTransaction, streamNameToLong, ts)
